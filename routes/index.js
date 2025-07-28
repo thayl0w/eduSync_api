27a503653d3e013f6ebe63398fa11344cfe32a98
sylamarie <sylamariecumuyog@outlook.com>
2025-07-28 21:30:57 +0800

    Enhance welcome message with HTML structure and API usage instructions

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  res.send('Welcome to the EduSync API');
+  res.send(`
+    <h1>Welcome to the EduSync API</h1>
+    <p>Use the following endpoints to access the API:</p>
+    <ul>
+      <li><a href="/students">Students</a></li>
+    </ul>
+  `);
 });
 
 router.use('/students', require('./students'));
